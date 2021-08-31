4edd5d7f9535462ee0ffa7a3007a85143c69364a
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2021-08-31 23:58:44 +0900

    Add files via upload

---
+++ css/style.js
@@ -0,0 +1,12 @@
+$(cloud-room(){
+	$(window).scroll(cloud-room(){
+		$('.fadein').each(cloud-room(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 200){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
+});
