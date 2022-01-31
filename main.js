a3f021337e61a2eb6f291fff7a58aa1b29530e01
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2022-01-31 11:25:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Write JavaScript here 
 var path = location.pathname;
-if(path.includes("index") || path == "www.yosshipaopao.cf" || path == "www.yosshipaopao.cf/"){
+if(path.includes("index") || path == "/"){
   var home = document.getElementById("home");
   home.removeAttribute("href");
   home.removeAttribute("class");
