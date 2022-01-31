17438cac19d461dd00efe8126aa168a78bf21b63
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2022-01-31 10:43:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Write JavaScript here 
 var path = location.pathname;
-if(path.includes("index") ){
+if(path.includes("index") || path == "www.yosshipaopao.cf" || path == "www.yosshipaopao.cf/"){
   var home = document.getElementById("home");
   home.removeAttribute("href");
   home.removeAttribute("class");
