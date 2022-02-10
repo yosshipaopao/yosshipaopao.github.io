2951e0bab148c8463f7b9885c2e00d44d574a470
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2022-02-10 17:05:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+$(function(){
 var script = document.createElement('script'); //変数名は適当なものにでも
 script.src = "https://code.jquery.com/jquery-3.6.0.min.js"; //ファイルパス
 document.head.appendChild(script); //<head>に生成
-
 window.onload = function() {
+  var cur = document.createElement('div');
+  cur.className = "cursor";
+  document.body.appendChild(cur); //<head>に生成
+  
   const spinner = document.getElementById('loading');
   spinner.classList.add('loaded');
   const obj = document.getElementById(spinner);
   obj.classList.add("cursor");
   obj.classList.remove("spinner");
 
-$(function(){
+
 	
 	//カーソル要素の指定
 	var cursor=$(".cursor");
