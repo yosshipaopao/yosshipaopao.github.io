252425ad0792cd469c5c53d857bada4151dc3e4a
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2022-02-18 01:13:54 +0900

    Update nyancat.js

--- js/nyancat.js
+++ js/nyancat.js
@@ -1,6 +1,6 @@
  window.addEventListener('load', function(){  
   var spinner = document.getElementById('back');
   spinner.classList.add('loaded');
-  var rainbow = document.getElementByClassName('del');
+  var rainbow = document.getElementById('del');
   rainbow.classList.add('loaded');
  });
