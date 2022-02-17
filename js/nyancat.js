13cbaa8fbfda1981dc52b50fd651b0e8bab17174
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2022-02-17 14:47:55 +0900

    Create nyancat.js

---
+++ js/nyancat.js
@@ -0,0 +1,6 @@
+ window.addEventListener('load', function(){  
+  const spinner = document.getElementById('back');
+  spinner.classList.add('loaded');
+  const rainbow = document.getElementById('rainbow');
+  rainbow.classList.add('loaded');
+ });
